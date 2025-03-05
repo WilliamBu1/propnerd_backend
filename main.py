1c2120bcf76ecba2f061a32b64b440e2340cbf9f
WilliamBu1 <williambu1027@gmail.com>
2025-03-04 21:54:14 -0500

    more cors

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://propnerd.vercel.app"],  # Add your frontend URLs
+    allow_origins=["http://localhost:5173", "https://propnerd-dev.vercel.app/"],  # Add your frontend URLs
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
