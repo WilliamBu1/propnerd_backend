9302c124d9a8e0f334ba2df8ec6a3f863a9c6528
WilliamBu1 <williambu1027@gmail.com>
2025-03-04 21:51:35 -0500

    added cors config

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "https://propnerd.vercel.app"],  # Add your frontend URLs
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/hello")
 def hello():
     return {"message": "Hello World"}
+
